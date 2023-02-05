a995cc0f9add3646d191623f49b032f6f8ab71cd
Tomas Pereira <tomaspereira2k@gmail.com>
2023-02-05 12:13:34 -0300

    optimization: now uses only 2 states instead of 9!

--- src/Components/Searchbar/WeatherModal.js
+++ src/Components/Searchbar/WeatherModal.js
@@ -3,38 +3,48 @@ import {useEffect, useState} from "react";
 function WeatherModal({isOpen, lat, lon, tempUnits}){
     
     const regex = /12:00:00/;
+
     let weatherFetchUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f583d2e3612de8b62a858c176aa37575&units=${tempUnits}`;
     let forecastFetchUrl =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=f583d2e3612de8b62a858c176aa37575&cnt=24&units=${tempUnits}`;
 
-    const [city, setCity] = useState("");
-    const [time, setTime] = useState("");
-    const [weather, setWeather] = useState("");
-    const [icon, setIcon] = useState("");
-    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    const [temp, setTemp] = useState("");
+    const [fetchedData, setFetchedData] = useState({
+        city: "",
+        time: "",
+        weather: "",
+        maxMin: "",
+        temp: "",
+        icon: "",
+    });
 
-    const [forecastIcon, setForecastIcon] = useState("");
-    const forecastIconUrl = `https://openweathermap.org/img/wn/${forecastIcon}@2x.png`;
-    const [forecastWeather, setForecastWeather] = useState("")
-    const [maxMin, setMaxMin] = useState([null, null]);
-    const [forecast, setForecast] = useState([]);
+    const [fetchedForecastData, setFetchedForecastData] = useState({
+        forecastIcon: "",
+        forecastWeather: "",
+        maxMin: [null, null],
+        forecast: [],
+    });
+
+    const iconUrl = `https://openweathermap.org/img/wn/${fetchedData.icon}@2x.png`;
+    const forecastIconUrl = `https://openweathermap.org/img/wn/${fetchedForecastData.forecastIcon}@2x.png`;
+
     let threeDayForecast = []; //this array is nesesary to use the map method
 
     const today = new Date();
     const utcTimeHours = today.getUTCHours();
     let utcTimeMinutes =  today.getUTCMinutes(); //universal time doesn´t change the minutes, only hours
-    let hours = utcTimeHours + (time / 3600);
+    let hours = utcTimeHours + (fetchedData.time / 3600);
 
     async function weatherFetch(){
         try{
             await fetch(weatherFetchUrl)
             .then((response) => response.json())
             .then((data) => {
-                setCity(`${data.name}, ${data.sys.country}`);
-                setTime(data.timezone);
-                setWeather(data.weather[0].main);
-                setTemp(Math.round(data.main.temp));
-                setIcon(data.weather[0].icon);
+                setFetchedData({
+                    city: `${data.name}, ${data.sys.country}`,
+                    time: data.timezone,
+                    weather: data.weather[0].main,
+                    temp: Math.round(data.main.temp),
+                    icon: data.weather[0].icon,
+                })
             })
         }
         catch(error){
@@ -47,10 +57,12 @@ function WeatherModal({isOpen, lat, lon, tempUnits}){
             await fetch (forecastFetchUrl)
             .then((response) => response.json())
             .then ((data) => {
-                setForecast(data.list)
-                setForecastIcon(data.list[0].weather[0].icon);
-                setMaxMin([Math.round(data.list[0].main.temp_max), Math.round(data.list[0].main.temp_min)]);
-                setForecastWeather(data.list[0].weather[0].main);
+                setFetchedForecastData({
+                    forecastIcon: data.list[0].weather[0].icon,
+                    forecastWeather: data.list[0].weather[0].main,
+                    maxMin: [Math.round(data.list[0].main.temp_max), Math.round(data.list[0].main.temp_min)],
+                    forecast: data.list,
+                });
             });
 
         }
@@ -58,9 +70,9 @@ function WeatherModal({isOpen, lat, lon, tempUnits}){
             console.log(error);
         };
     };
-
+    
     if(hours < 0){
-      hours = (hours + time) * -1;
+      hours = (hours + fetchedData.time) * -1;
     };
 
     if(hours > 24){
@@ -80,12 +92,12 @@ function WeatherModal({isOpen, lat, lon, tempUnits}){
         forecastFetch();
     }, [lat]);
 
-    if(forecast){
-        for(let i = 0; i < forecast.length; i++){
-            if(regex.test(forecast[i].dt_txt)){
-                threeDayForecast.push(forecast[i]);
+    if(fetchedForecastData.forecast){
+        for(let i = 0; i < fetchedForecastData.forecast.length; i++){
+            if(regex.test(fetchedForecastData.forecast[i].dt_txt)){
+                threeDayForecast.push(fetchedForecastData.forecast[i]);
                 i++
-            }else if(!forecast[i].dt_txt){
+            }else if(!fetchedForecastData.forecast[i].dt_txt){
                 i++
             };
         };
@@ -109,18 +121,18 @@ function WeatherModal({isOpen, lat, lon, tempUnits}){
             <div className="current-weather-container">
                 <div className="text-container">
                     <h2>Current weather in: </h2>
-                    <h2>{city} | {`${hours}:${utcTimeMinutes}`}</h2>
+                    <h2>{fetchedData.city} | {`${hours}:${utcTimeMinutes}`}</h2>
                 </div>
                 <div className="icon-and-temp">
-                    <img className="current-icon" src={iconUrl} alt={weather}/>
-                    <h2>{weather} {temp}º</h2>
+                    <img className="current-icon" src={iconUrl} alt={fetchedData.weather}/>
+                    <h2>{fetchedData.weather} {fetchedData.temp}º</h2>
                 </div>
             </div>
             <div className="three-hours-forecast-container">
                 <div className="forecast-icon-and-temp ">
                     <h2>weather for the next 3 hours</h2>
                     <img src={forecastIconUrl} alt="weather for the next 3 hours"/>
-                    <h2>{forecastWeather} {maxMin[0]}º max | {maxMin[1]}º min</h2>
+                    <h2>{fetchedForecastData.forecastWeather} {fetchedForecastData.maxMin[0]}º max | {fetchedForecastData.maxMin[1]}º min</h2>
                 </div>
             </div>
             <div className="three-days-forecast-container">
